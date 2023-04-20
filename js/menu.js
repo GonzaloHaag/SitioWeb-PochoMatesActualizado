13ce629de5ca19e40e354a7d0f74dd4627b71d9d
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-04-20 13:12:59 -0300

    Cambios al menu

--- js/menu.js
+++ js/menu.js
@@ -11,3 +11,25 @@ openMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
 })
+
+/*Button para mostrar categoria de mates pampas y camioneros(imperiales) */
+const buttonDesplegarMates = document.querySelector("#mostrarMates");
+const divMenuMates = document.querySelector(".menu-mates");
+const buttonOcultarMates = document.querySelector("#ocultar-mates");
+const buttonTermos = document.querySelector("#combos");
+
+buttonDesplegarMates.addEventListener("click",()=>{
+    divMenuMates.classList.add("menu-mates-visible");
+
+
+    buttonTermos.classList.add("margin");
+    buttonDesplegarMates.classList.add("disabled");
+    buttonOcultarMates.classList.remove("disabled");
+   
+})
+buttonOcultarMates.addEventListener("click",()=>{
+    divMenuMates.classList.remove("menu-mates-visible");
+    buttonDesplegarMates.classList.remove("disabled");
+    buttonOcultarMates.classList.add("disabled");
+    buttonTermos.classList.remove("margin");
+})
