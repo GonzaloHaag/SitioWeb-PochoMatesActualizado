a1120379aa7a28dd5ecfd014eb045da106cbe9e3
Gonzalo Haag <gonzalohaag13@gmail.com>
2023-04-23 15:27:02 -0300

    Agregamos secciones,ver donde colocar footer

--- js/menu.js
+++ js/menu.js
@@ -14,22 +14,58 @@ closeMenu.addEventListener("click", () => {
 
 /*Button para mostrar categoria de mates pampas y camioneros(imperiales) */
 const buttonDesplegarMates = document.querySelector("#mostrarMates");
+const buttonDesplegarAccesorios = document.querySelector("#mostrarAccesorios");
 const divMenuMates = document.querySelector(".menu-mates");
+const divMenuAccesorios = document.querySelector(".menu-accesorios");
 const buttonOcultarMates = document.querySelector("#ocultar-mates");
-const buttonTermos = document.querySelector("#combos");
+const buttonOcultarAccesorios = document.querySelector("#ocultarAccesorios");
+const buttonCombos = document.querySelector("#combos");
+const buttonVasosJarras = document.getElementById("vasos-jarras");
+const buttonTermos = document.getElementById("termos");
+const buttonAccesorios = document.querySelector(".accesorios");
+const yerberas = document.getElementById("yerberas");
+const materas = document.getElementById("materas");
+const bombillas = document.getElementById("bombillas");
+const bolucompras = document.getElementById("bolucompras");
+
 
 buttonDesplegarMates.addEventListener("click",()=>{
     divMenuMates.classList.add("menu-mates-visible");
 
 
-    buttonTermos.classList.add("margin");
+    buttonCombos.classList.add("margin");
     buttonDesplegarMates.classList.add("disabled");
     buttonOcultarMates.classList.remove("disabled");
+    buttonVasosJarras.classList.add("translate");
+    buttonTermos.classList.add("translate");
+    buttonAccesorios.classList.add("translate");
+    yerberas.classList.add("translate");
+    materas.classList.add("translate");
+    bombillas.classList.add("translate");
+    bolucompras.classList.add("translate");
    
 })
 buttonOcultarMates.addEventListener("click",()=>{
     divMenuMates.classList.remove("menu-mates-visible");
     buttonDesplegarMates.classList.remove("disabled");
     buttonOcultarMates.classList.add("disabled");
-    buttonTermos.classList.remove("margin");
+    buttonCombos.classList.remove("margin");
+    buttonVasosJarras.classList.remove("translate");
+    buttonTermos.classList.remove("translate");
+    buttonAccesorios.classList.remove("translate");
+    yerberas.classList.remove("translate");
+    materas.classList.remove("translate");
+    bombillas.classList.remove("translate");
+    bolucompras.classList.remove("translate");
+})
+
+buttonDesplegarAccesorios.addEventListener("click",() => {
+    divMenuAccesorios.classList.add("menu-accesorios-visible");
+    buttonDesplegarAccesorios.classList.add("disabled");
+    buttonOcultarAccesorios.classList.remove("disabled");
+});
+buttonOcultarAccesorios.addEventListener("click",()=>{
+    divMenuAccesorios.classList.remove("menu-accesorios-visible");
+    buttonDesplegarAccesorios.classList.remove("disabled");
+    buttonOcultarAccesorios.classList.add("disabled");
 })
